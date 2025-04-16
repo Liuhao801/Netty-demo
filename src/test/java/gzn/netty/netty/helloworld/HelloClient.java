5e59f21d768ac39eb4b8b829816affd395c0f71c
gzn <13776840962@163.com>
2025-04-16 14:05:17 +0800

    netty入门

---
+++ src/test/java/gzn/netty/netty/helloworld/HelloClient.java
@@ -0,0 +1,27 @@
+package gzn.netty.netty.helloworld;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()  //阻塞方法，直到连接建立
+                .channel()  //代表连接对象
+                .writeAndFlush("hello, world");
+    }
+}
